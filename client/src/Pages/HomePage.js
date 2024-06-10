bff9770c9ac0b948e4e944e7c5359f8c409503db
shivam bole <shivamofficial285@gmail.com>
2024-06-10 13:19:28 +0530

    make button large

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -76,6 +76,53 @@ function HomePage() {
         <ProductComponent />
         <FrozenSnacks/>
     <PopularBrand/>
+
+    <div className="w-[95vw] ml-5 mt-2 bottom-0 radius" >
+        <Slider {...settings} className="radius">
+          <div className="w-full radius">
+            <a href="./product/1 radius">
+              <div className="flex items-center  justify-end bg-opacity-50 radius w-full">
+                <img
+                  src="./skin_care.jpg"
+                  className=" object-fill h-[60vh] w-full radius"
+                 
+                ></img>
+              </div>
+            </a>
+          </div>
+          <div className="w-full radius">
+            <a href="./product/2 radius" >
+              <div className="flex items-center  justify-end bg-opacity-50 rounded radius w-full">
+                <img
+                  src="./chocolates.jpg"
+                  className=" object-fill h-[60vh] w-full radius"
+                ></img>
+              </div>
+            </a>
+          </div>
+          <div className="w-full radius">
+            <a href="./product/1">
+              <div className="flex items-center  justify-end bg-opacity-50 radius w-full">
+                <img
+                  src="./dailyneeds.jpg"
+                  className=" object-fill h-[60vh] w-full radius"
+                ></img>
+              </div>
+            </a>
+          </div>
+          <div className="w-full radius">
+            <a href="./product/1">
+              <div className="flex items-center  justify-end bg-opacity-50 radius  w-full">
+                <img
+                  src="./snacks.jpg"
+                  className=" object-fill h-[60vh] w-full radius"
+                ></img>
+              </div>
+            </a>
+          </div>
+      
+        </Slider>
+      </div>
     
       <Footer/>
     </div>

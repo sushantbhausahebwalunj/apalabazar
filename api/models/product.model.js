05154fee70a9c2e802f40e8681121c64c1e14647
Ankit Rai <ankitrai5096@gmail.com>
2024-07-02 14:34:28 +0530

    Change the product details page

--- api/models/product.model.js
+++ api/models/product.model.js
@@ -9,14 +9,14 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  price:{
+  price: {
     type: Number,
     required: true,
   },
-  discountedPrice:{
+  discountedPrice: {
     type: Number,
   },
-  discountPersent: {
+  discountPercent: {
     type: Number,
   },
   quantity: {
@@ -24,33 +24,30 @@ const productSchema = new mongoose.Schema({
   },
   brand: {
     type: String,
-  }, 
-  imageUrl:{
+  },
+  imageUrl: {
     type: String,
   },
-  ratings: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'ratings',
-    },
-  ],
+  ratings: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'ratings',
+  }],
   reviews: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'reviews',
+  }],
+  numRatings: {
+    type: Number,
+    default: 0,
   },
-],
-numRatings: {
-  type: Number,
-  default: 0,
-},
-category: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: 'categories',
-},
-createdAt: {
-  type: Date,
-  default: Date.now,
-},
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'categories',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Product = mongoose.model("products", productSchema);

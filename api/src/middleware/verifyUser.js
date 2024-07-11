467758c072e372bba2a3bd52496663618d776a89
SahilChaudhari <sahilchaudhari@SahilAir.local>
2024-07-11 10:34:26 +0530

    remove some testing commit

--- api/src/middleware/verifyUser.js
+++ api/src/middleware/verifyUser.js
@@ -3,8 +3,7 @@ import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
-    console.log(token);
-//    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2ODE4MTk1M2I1M2ViY2E4ZjRiMTRkNSIsInJvbGUiOiJDVVNUT01FUiIsImVtYWlsIjoibWFoZXNoMTIzQGdtYWlsLmNvbSIsImZpcnN0TmFtZSI6Ik1haGVzaCIsImxhc3ROYW1lIjoiU2hpbmRlIiwiaWF0IjoxNzIwNjczMTQ0LCJleHAiOjE3MjA2NzY3NDR9.yM9Oz0uch0xPWJL_QgatGNxLln9-m96oF24TaB-98bY";
+ 
     if (!token) return next(errorHandler(401, 'Unauthorized'));
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {

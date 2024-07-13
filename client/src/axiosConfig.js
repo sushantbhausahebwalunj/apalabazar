6899e93169fc693be816a417e856bba5dd3f8036
SahilChaudhari <sahilchaudhari@SahilAir.local>
2024-07-13 21:19:28 +0530

    fix port and ip mapping

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5454/api', // Change this to your API base URL
+  baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5454/api',// Change this to your API base URL
   withCredentials: true, // Include credentials in requests
 });
 
 export default axiosInstance;
+ 

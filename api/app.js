aa891c848457c6e0e5a3bab9b21ffdebcc35f4b3
sahilchaudhari2405 <sahilchaudhari2405@gmail.com>
2024-07-17 01:11:00 +0530

    Add file fix bug

--- api/app.js
+++ api/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 const url = process.env.BASE_URL || 'http://localhost:3000'
 app.use(cors({
-  origin: '*', // Allow all origins
+  origin: url, // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Enable credentials

1063c16c919c90f572650916e60ec3c1c4668c1d
sahilchaudhari2405 <sahilchaudhari2405@gmail.com>
2024-07-17 00:02:40 +0530

    Add file changes

--- api/app.js
+++ api/app.js
@@ -3,31 +3,28 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import allRouter from './src/Router/router.js';
 import morgan from 'morgan';
+
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(morgan('dev')); 
-
-
-
-const allowedOrigin = process.env.BASE_URL || 'http://localhost:3000';
+app.use(morgan('dev'));
 
 app.use(cors({
-    origin: allowedOrigin,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Enable credentials
-  }));
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Enable credentials
+}));
 
 // Apply JWT authentication middleware to all routes
 // app.use(jwtAuth);
 
-app.use('/api', allRouter);                                                                 
+app.use('/api', allRouter);
 
 app.get("/", (req, res) => {
-    return res.status(200).send({ message: "Welcome to Apala Bazar API - Node", status: true });
+  return res.status(200).send({ message: "Welcome to Apala Bazar API - Node", status: true });
 });
 
 export default app;

6ad7ec07d19493b14c873470ec8796e2f4f3a125
sahilchaudhari2405 <sahilchaudhari2405@gmail.com>
2024-07-17 01:21:57 +0530

    Add file fix bug

--- api/app.js
+++ api/app.js
@@ -8,11 +8,23 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-
 app.use(morgan('dev'));
-const url = process.env.BASE_URL || 'http://localhost:3000'
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://apalabajar.com',
+  'http://www.apalabajar.com'
+];
+
 app.use(cors({
-  origin: url, // Allow all origins
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps or curl requests)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Enable credentials

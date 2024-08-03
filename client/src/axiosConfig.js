610d4abb4b06effe21a9ebbb25592e35d9be5a1e
sahil chaudhari <82030235+sahilchaudhari2405@users.noreply.github.com>
2024-08-03 16:48:06 +0530

    try to add local commit

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:5454/api',// Change this to your API base URL
+  baseURL:process.env.REACT_APP_BASE_URL ||  'http://localhost:5454/api',// Change this to your API base URL
   withCredentials: true, // Include credentials in requests
 });
-
+  
 export default axiosInstance;
  

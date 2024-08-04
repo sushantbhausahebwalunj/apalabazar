ed997c2ab37194338b55a1378f184faed0be4e3a
sahil chaudhari <82030235+sahilchaudhari2405@users.noreply.github.com>
2024-08-04 20:03:03 +0530

    changes add on config file

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:process.env.REACT_APP_BASE_URL ||  'http://localhost:5454/api',// Change this to your API base URL
+  baseURL: 'http://0.0.0.0:5454/api',// Change this to your API base URL
   withCredentials: true, // Include credentials in requests
 });
   
